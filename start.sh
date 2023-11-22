fe0684aaf91f2db4b787e114e9cbfadb9bf8fec7
Chris Shenton <chris@v-studios.com>
2023-11-22 16:49:04 +0100

    VPCEndpoint required, tighten S3 Bucket and Object permissions

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-# Start Django, after first doing a migration and setting root user password.
+# If the function is Janitor, initialize; else start Django server.
 
 echo "## ${0} WAGTAIL_JANITOR=${WAGTAIL_JANITOR}"
 if [ -z "${WAGTAIL_JANITOR}" ]
@@ -9,11 +9,14 @@ then
 else
     ./s3check.py
 
-    # echo "## ${0} Migrate..."
-    # ./manage.py migrate
-    # echo "## ${0} Superuser..."
-    # DJANGO_SUPERUSER_PASSWORD=KILLME ./manage.py createsuperuser --noinput --username chris --email chris@v-studios.com || echo "COULD NOT SET SUPERUSER, MAYBE ALREADY SET"
+    echo "## ${0} Migrate..."
+    ./manage.py migrate
+
+    echo "## ${0} Superuser..."
+    DJANGO_SUPERUSER_PASSWORD=KILLME ./manage.py createsuperuser --noinput --username chris --email chris@v-studios.com || echo "COULD NOT SET SUPERUSER, MAYBE ALREADY SET"
+
     echo "## ${0} Collect Static..."
+    # --clear flag throws error
     ./manage.py collectstatic --noinput -v 3
 fi
 
